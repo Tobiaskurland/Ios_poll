b503c8bd32feaec38c203c778f4440423296e07a
Tobias <tobias.ku22@gmail.com>
2019-11-01 09:51:40 +0100

    first commit

--- LocationApp/ViewController.swift
+++ LocationApp/ViewController.swift
@@ -1,20 +1,76 @@
-//
-//  ViewController.swift
-//  LocationApp
-//
-//  Created by Tobias Kurland on 25/10/2019.
-//  Copyright © 2019 Tobias Kurland. All rights reserved.
-//
+
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    
+    @IBOutlet weak var viewTable: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
     }
+    
+    
+@IBAction func startFind(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "20CAE8A0-A9CF-11E3-A5E2-0800200C9A66")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 234, minor: 55967, identifier: "MyBeacon")
+
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+            print(beacons[0].proximity.rawValue)
+        } else {
+            updateDistance(.unknown)
+            print("is 0")
+        }
+    }
+
+    func updateDistance(_ distance: CLProximity) {
+        
+        UIView.animate(withDuration: 2.5) {
+            switch distance {
+            case .unknown:
+                self.viewTable.backgroundColor = UIColor.gray
+                    print("unkown")
+            case .far:
+                self.viewTable.backgroundColor = UIColor.blue
+                    print("far")
+            case .near:
+                self.viewTable.backgroundColor = UIColor.green
+                    print("near")
+                    
+            case .immediate:
+                self.viewTable.backgroundColor = UIColor.red
+                    print("immediate")
+            }
+        }
+    }
+    
+
 
 
 }
-
